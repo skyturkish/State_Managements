6d7a6d15021463cd520ac5b12fa338093bacd740
skyturkish <gokturk.acr2002@gmail.com>
2022-07-25 12:58:19 +0300

    Listview using with vanilla state management

--- vanilla/lib/main.dart
+++ vanilla/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() => runApp(
       const MyApp(),
@@ -20,31 +21,47 @@ class MyApp extends StatelessWidget {
 }
 
 class Contact {
+  final String id;
   final String name;
   Contact({
     required this.name,
-  });
+  }) : id = const Uuid().v4();
 }
 
-class ContactBook {
+// ---------------------We provide a type which we want to listen
+class ContactBook extends ValueNotifier<List<Contact>> {
   // Singleton
-  ContactBook._sharedInstance();
+  ContactBook._sharedInstance() : super([Contact(name: 'Hello')]);
   static final ContactBook _shared = ContactBook._sharedInstance();
   factory ContactBook() => _shared;
 
-  final List<Contact> _contacts = [Contact(name: 'Sky')];
-
-  int get length => _contacts.length;
+  int get length => value.length;
 
   void add({required Contact contact}) {
-    _contacts.add((contact));
+    // We're actually changing not the value itself but er're changing the internals of value
+    // so this doesn't this code sends no seetter signal to our change notifier
+
+    // value.add((contact));
+    // notifyListeners(); That would work
+
+    // Or
+
+    final concats = value;
+    concats.add(contact);
+    //value = concats; will delete
+    notifyListeners();
   }
 
   void remove({required Contact contact}) {
-    _contacts.remove((contact));
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      //value = contacts;
+      notifyListeners();
+    }
   }
 
-  Contact? contact({required int atIndex}) => _contacts.length > atIndex ? _contacts[atIndex] : null;
+  Contact? contact({required int atIndex}) => value.length > atIndex ? value[atIndex] : null;
 }
 
 class HomeView extends StatefulWidget {
@@ -57,17 +74,34 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    final contackBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView Bar'),
       ),
-      body: ListView.builder(
-        itemCount: contackBook.length,
-        itemBuilder: (context, index) {
-          final contact = contackBook.contact(atIndex: index)!;
-          return ListTile(
-            title: Text(contact.name),
+      body: ValueListenableBuilder(
+        valueListenable: ContactBook(),
+        builder: (context, List<Contact> value, child) {
+          final contacts = value;
+          return ListView.builder(
+            itemCount: contacts.length,
+            itemBuilder: (context, index) {
+              final contact = contacts[index];
+              return Dismissible(
+                onDismissed: (direction) {
+                  //contacts.remove(contact);
+                  // Or
+                  ContactBook().remove(contact: contact);
+                },
+                key: ValueKey(contact.id),
+                child: Material(
+                  color: Colors.white,
+                  elevation: 6.6,
+                  child: ListTile(
+                    title: Text(contact.name),
+                  ),
+                ),
+              );
+            },
           );
         },
       ),
@@ -89,6 +123,8 @@ class NewContantView extends StatefulWidget {
 }
 
 class NewContantViewState extends State<NewContantView> {
+  final _formKey = GlobalKey<FormState>();
+
   late final TextEditingController _controller;
 
   @override
@@ -112,21 +148,24 @@ class NewContantViewState extends State<NewContantView> {
           style: TextStyle(color: Colors.red),
         ),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _controller,
-            decoration: const InputDecoration(hintText: 'Enter a new contact name here... '),
-          ),
-          TextButton(
-            onPressed: () {
-              final contact = Contact(name: _controller.text);
-              ContactBook().add(contact: contact);
-              Navigator.of(context).pop();
-            },
-            child: const Text('Add contact'),
-          )
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(hintText: 'Enter a new contact name here... '),
+            ),
+            TextButton(
+              onPressed: () {
+                final contact = Contact(name: _controller.text);
+                ContactBook().add(contact: contact);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Add contact'),
+            )
+          ],
+        ),
       ),
     );
   }
