6a4f02333b15e48db412433a56e6bcde059c0d83
skyturkish <gokturk.acr2002@gmail.com>
2022-07-28 01:47:07 +0300

    changeTheme

---
+++ provider/examples/changetheme/android/app/src/main/kotlin/sky/sky/changetheme/MainActivity.kt
@@ -0,0 +1,6 @@
+package sky.sky.changetheme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ provider/examples/changetheme/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeNotifier>(
+          create: (context) => ThemeNotifier(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: context.watch<ThemeNotifier>().currentTheme,
+      title: 'Material App',
+      home: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView Bar'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              context.read<ThemeNotifier>().changeTheme();
+            },
+            child: const Text('Change Theme')),
+      ),
+    );
+  }
+}
+
+enum AppThemes { LIGHT, DARK }
+
+class ThemeNotifier extends ChangeNotifier {
+  ThemeData _currentTheme = ThemeData.light();
+
+  AppThemes _currentThemeEnum = AppThemes.LIGHT;
+
+  /// Applicaton theme enum.
+  /// Deafult value is [AppThemes.LIGHT]
+  AppThemes get currenThemeEnum => _currentThemeEnum;
+
+  ThemeData get currentTheme => _currentTheme;
+
+  /// Change your app theme with [currenThemeEnum] value.
+  void changeTheme() {
+    if (_currentThemeEnum == AppThemes.LIGHT) {
+      _currentTheme = ThemeData.dark();
+      _currentThemeEnum = AppThemes.DARK;
+    } else {
+      _currentTheme = ThemeData.light();
+      _currentThemeEnum = AppThemes.LIGHT;
+    }
+    notifyListeners();
+  }
+}
