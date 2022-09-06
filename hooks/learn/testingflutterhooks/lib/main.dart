dbbf1476e8cb67db8a8e1faf9ab4cdecdc5af584
skyturkish <gokturk.acr2002@gmail.com>
2022-09-06 16:05:27 +0300

    hooks rotation image

--- hooks/learn/testingflutterhooks/lib/main.dart
+++ hooks/learn/testingflutterhooks/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -7,12 +6,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'dart:developer' as devtools show log;
 
 // for more information about CompactMap => https://www.hackingwithswift.com/articles/205/whats-the-difference-between-map-flatmap-and-compactmap
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(transform ?? (e) => e).where((e) => e != null).cast();
-}
 
 extension Log on Object {
   void log() => devtools.log(toString());
@@ -22,24 +15,6 @@ void main() {
   runApp(
     const MyApp(),
   );
-}
-
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription streamSubscription;
-
-  CountDown({required int from}) : super(from) {
-    streamSubscription = Stream.periodic(
-      const Duration(seconds: 1),
-      (v) => from - v,
-    ).takeWhile((value) => value >= 0).listen((value) {
-      this.value = value;
-    });
-  }
-  @override
-  void dispose() {
-    streamSubscription.cancel();
-    super.dispose();
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -54,87 +29,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<String> getTime() => Stream.periodic(const Duration(seconds: 1), (_) {
-      return DateTime.now().toIso8601String();
-    });
-
 const String url = 'https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg';
-const imageHeight = 300.0;
-
-extension Normalize on num {
-  num normalized(
-    num selfRangeMin,
-    num selfRangeMax, [
-    num normalizedRangeMin = 0.0,
-    num normalizedRangeMax = 1.0,
-  ]) =>
-      (normalizedRangeMax - normalizedRangeMin) * ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
-      normalizedRangeMin;
-}
 
 class HomeView extends HookWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    'allah aşkına şurayı kaç defa çağırılıyor'.log();
-    final opacity = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-
-    final size = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-
-    final scrollController = useScrollController();
-    useEffect(() {
-      scrollController.addListener(() {
-        final newOpacity = max(imageHeight - scrollController.offset, 0.0);
-        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
-        opacity.value = normalized;
-        size.value = normalized;
-      });
-      return null;
-    }, [scrollController]);
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(onListen: () {
+      controller.sink.add(0.0);
+    });
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home VView'),
-      ),
-      body: Column(
-        children: [
-          SizeTransition(
-            axisAlignment: -1.0,
-            axis: Axis.vertical,
-            sizeFactor: size,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                url,
-                height: imageHeight,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: scrollController,
-              itemCount: 100,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Person ${index + 1}'),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Home VView'),
+        ),
+        body: StreamBuilder<double>(
+          stream: controller.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            } else {
+              final rotation = snapshot.data ?? 0.0;
+              return GestureDetector(
+                onTap: () {
+                  controller.sink.add(rotation + 90.0);
+                },
+                child: RotationTransition(
+                  turns: AlwaysStoppedAnimation(rotation / 360.0),
+                  child: Center(
+                    child: Image.network(url),
+                  ),
+                ),
+              );
+            }
+          },
+        ));
   }
 }
