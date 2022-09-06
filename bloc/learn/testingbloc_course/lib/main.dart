73bf77b6298f6b8e9280c4c2dca970c4d56f49d5
skyturkish <gokturk.acr2002@gmail.com>
2022-09-06 15:53:05 +0300

    hooks image fade size

--- bloc/learn/testingbloc_course/lib/main.dart
+++ bloc/learn/testingbloc_course/lib/main.dart
@@ -1,18 +1,4 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testingbloc_course/bloc/bloc_actions.dart';
-import 'package:testingbloc_course/bloc/person.dart';
-
-import 'dart:developer' as devtools show log;
-
-import 'package:testingbloc_course/bloc/persons_bloc.dart';
-
-extension Log on Object {
-  void log() => devtools.log(toString());
-}
 
 void main() {
   runApp(
@@ -25,27 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: BlocProvider(
-        create: (_) => PersonsBloc(),
-        child: const HomeView(),
-      ),
+      home: HomeView(),
     );
   }
-}
-
-// http ile basit bir get işlemi ve sürekli convert, paketsiz bu
-// ara sıra paketsiz yap şunları
-Future<Iterable<Person>> getPersons(String url) => HttpClient()
-    .getUrl(Uri.parse(url))
-    .then((req) => req.close())
-    .then((resp) => resp.transform(utf8.decoder).join())
-    .then((str) => json.decode(str) as List<dynamic>)
-    .then((list) => list.map((e) => Person.fromJson(e)));
-
-extension Subscript<T> on Iterable<T> {
-  T? operator [](int index) => length > index ? elementAt(index) : null;
 }
 
 class HomeView extends StatefulWidget {
@@ -62,51 +32,8 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: const Text('HomeView Bar'),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {
-                  context.read<PersonsBloc>().add(const LoadPersonsAction(url: persons1Url, loader: getPersons));
-                },
-                child: const Text('Load json #1'),
-              ),
-              TextButton(
-                onPressed: () {
-                  context.read<PersonsBloc>().add(const LoadPersonsAction(url: persons2Url, loader: getPersons));
-                },
-                child: const Text('Load json #2'),
-              ),
-            ],
-          ),
-          BlocBuilder<PersonsBloc, FetchResult?>(
-            buildWhen: (previousResult, currentResult) {
-              return previousResult?.persons != currentResult?.persons;
-            },
-            builder: (context, fetchResult) {
-              fetchResult?.log();
-              final persons = fetchResult?.persons;
-              if (persons == null) {
-                return const SizedBox();
-              }
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: persons.length,
-                  itemBuilder: (context, index) {
-                    final person = persons[
-                        index]; // yukarı yazdığımız extension sayesinde sayı verip o indexteki değeri alabiliyoruz
-                    return ListTile(
-                      title: Text(
-                        person?.name ?? 'elemanın ismi yok usta',
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
-          )
-        ],
+      body: const Center(
+        child: Text('Hello World'),
       ),
     );
   }
--- hooks/learn/testingflutterhooks/lib/main.dart
+++ hooks/learn/testingflutterhooks/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -58,20 +59,82 @@ Stream<String> getTime() => Stream.periodic(const Duration(seconds: 1), (_) {
     });
 
 const String url = 'https://i.pinimg.com/originals/aa/02/78/aa02780bbc7e6c5e2d52d9b0e919fbf6.jpg';
+const imageHeight = 300.0;
+
+extension Normalize on num {
+  num normalized(
+    num selfRangeMin,
+    num selfRangeMax, [
+    num normalizedRangeMin = 0.0,
+    num normalizedRangeMax = 1.0,
+  ]) =>
+      (normalizedRangeMax - normalizedRangeMin) * ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
+      normalizedRangeMin;
+}
 
 class HomeView extends HookWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    'yenilendi'.log();
-    final countDown = useMemoized(() => CountDown(from: 20));
-    final notifier = useListenable(countDown);
+    'allah aşkına şurayı kaç defa çağırılıyor'.log();
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+
+    final scrollController = useScrollController();
+    useEffect(() {
+      scrollController.addListener(() {
+        final newOpacity = max(imageHeight - scrollController.offset, 0.0);
+        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [scrollController]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home VView'),
       ),
-      body: Center(child: Text(notifier.value.toString())),
+      body: Column(
+        children: [
+          SizeTransition(
+            axisAlignment: -1.0,
+            axis: Axis.vertical,
+            sizeFactor: size,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Person ${index + 1}'),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
